f09297f97ef92acacd0f851dd35486ecb294cbae
Dinh Nhat Bang <dinhnhatbang@gmail.com>
2019-06-13 14:09:47 +0700

    update convert

--- crawler/src/convert.py
+++ crawler/src/convert.py
@@ -7,9 +7,18 @@ import pyarrow.parquet as pq
 input_file = sys.argv[1]
 output_dir = sys.argv[2]
 
+
+data_type = {
+    'id': int,
+    'retweet_count': int,
+    'full_text': str,
+    'created_at': str
+}
+
 data_frame = pd.read_json(
         path_or_buf=input_file,
-        dtype=data_type
+        dtype=data_type,
+        lines=True
     )
 
 table = pa.Table.from_pandas(df=data_frame)
